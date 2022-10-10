db121b9a84e3495ba2a6b35caf3f51e1f4fd084d
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-10 10:38:13 +0700

    edit

--- Backend/lang/en/passwords.php
+++ Backend/lang/en/passwords.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
+    'reset' => 'รีเซ็ตรหัสผ่านสำเร็จ',
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
